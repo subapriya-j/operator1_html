1a044c30f2f8d7aba5184e6b0a2d4ca4aee1b237
subapriya-j <ABC>
2024-01-21 18:31:35 +0530

    hfgdgkjh

---
+++ operator.js
@@ -0,0 +1,18 @@
+//Assignment operator
+var a=10
+console.log("The assignment operator",a)
+//Arithmetic operator
+var aA=10
+var bB=10
+console.log("The arithmetic operator of addition",aA+bB)
+console.log(typeof aA)
+console.log("The arithmetic operator of subtraction",aA-bB)
+console.log("The arithmetic operator of multiplication",aA*bB)
+console.log("The arithmetic operator of division",aA/bB)
+console.log("The arithmetic operator of modulo",aA%bB)
+//Logical operator
+var al=1
+var bl=30
+console.log("The logical operator &&",al&&bl)
+console.log("The logical operator ||",al||bl)
+console.log("The logical operator !",!al)
